5950bc58adb75b7364a2fe223e0f01ec14473620
erlams <elmaddin@gmail.com>
2023-11-18 19:19:04 +0100

    55trr5r

---
+++ script.js
@@ -0,0 +1,12 @@
+let city =  prompt ('WHAT IS THE CAPITA: US');
+let netice ;
+if (city == 'Washington' ) {
+
+
+netice = 'CAPITAL OF US';
+
+}else {
+    netice = 'NO NO TRUE'
+}
+
+document.getElementById('cit').innerHTML = netice;
